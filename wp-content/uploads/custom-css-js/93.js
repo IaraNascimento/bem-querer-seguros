af293590b204558033f7ae723c1ba84f40d76ad0
Gouvea <caua.nascimento@sptech.school>
2024-07-24 21:11:54 -0300

    header banana concluida

--- wp-content/uploads/custom-css-js/93.js
+++ wp-content/uploads/custom-css-js/93.js
@@ -42,6 +42,51 @@ function nextMSV() {
 	changeMSV();
 }
 
+function checkScroll(index) {
+	console.log(index);
+
+	const upperHeader = document.getElementsByClassName("main-header-bar")[index];
+	console.log(upperHeader);
+	const headerLogo = document.getElementsByClassName("custom-logo")[index];
+	const aboveHeader = document.getElementsByClassName("ast-above-header-bar")[index];
+
+	const textHeader = document.querySelectorAll('#ast-hf-menu-1 li a');
+	const linkHeader = document.querySelectorAll(".ast-header-social-1-wrap div a span svg path");
+
+	
+	if (window.scrollY < 48) {
+		console.log(upperHeader);
+		upperHeader.style.backgroundColor = "transparent";
+		headerLogo.src = "http://localhost/bem-querer-seguros/wp-content/uploads/2024/06/cropped-Marca-BemQuerer-Seguros-Laranja.png";
+		headerLogo.srcset = "http://localhost/bem-querer-seguros/wp-content/uploads/2024/06/cropped-Marca-BemQuerer-Seguros-Laranja.png";
+		aboveHeader.style.display = "block";
+		
+		textHeader.forEach(sessionButton => {
+			sessionButton.style.color = "var(--orange)";
+		})
+		linkHeader.forEach(linkButton => {
+			linkButton.setAttribute('fill', 'var(--orange)');
+		})
+	} else {
+		console.log(upperHeader);
+		upperHeader.style.backgroundColor = "var(--orange)";
+		headerLogo.src = "http://localhost/bem-querer-seguros/wp-content/uploads/2024/06/Marca-BemQuerer-Seguros-Branca.png";
+		headerLogo.srcset = "http://localhost/bem-querer-seguros/wp-content/uploads/2024/06/Marca-BemQuerer-Seguros-Branca.png";
+		aboveHeader.style.display = "none";
+		
+		textHeader.forEach(sessionButton => {
+			sessionButton.style.color = "white";
+		})
+		linkHeader.forEach(linkButton => {
+			linkButton.setAttribute('fill', 'white');
+		})
+	}
+}
+
+window.addEventListener("scroll", () => checkScroll(0));
+window.addEventListener("scroll", () => checkScroll(1));
+checkScroll(0);
+checkScroll(1);
 
 document.getElementById("previous-msv").addEventListener("click", previousMSV);
 document.getElementById("next-msv").addEventListener("click", nextMSV);</script>
